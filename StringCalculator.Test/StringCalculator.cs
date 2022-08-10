923152afd9133a2a61d5032e581f78d572f40469
alessio <alessio.rea@keyconsulting.fr>
2022-08-10 16:44:34 +0200

    progress

---
+++ StringCalculator.Test/StringCalculator.cs
@@ -0,0 +1,102 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StringCalculator.Test
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            var delimitors = GetStandardDelimitors();
+
+            if (input.HasCustomDelimitors())
+            {
+                delimitors.RegisterCustomDelimitors(input);
+                input = RemoveCustomDelimitorDeclarationFrom(input);
+            }
+
+            var numbers = input.ExtractNumbers(delimitors);
+            ThrowExceptionIfThereIsAnyNegativeNumber(numbers);
+            numbers = FilterOutNumbersHigherThanThreshold(numbers, 1000);
+
+            int sum = numbers.Sum();
+            return sum;
+        }
+
+        private static IEnumerable<int> FilterOutNumbersHigherThanThreshold(IEnumerable<int> numbers, int threshold)
+        {
+            return numbers.Where(n => n <= threshold);
+        }
+
+        private static void ThrowExceptionIfThereIsAnyNegativeNumber(IEnumerable<int> numbers)
+        {
+            IEnumerable<int> negativeNumbers = ExtractNegativeNumbers(numbers);
+            if (HasNegativeNumbers(negativeNumbers))
+            {
+                throw new Exception($"negatives not allowed: {String.Join(", ", negativeNumbers)}");
+            }
+        }
+
+        private static IEnumerable<int> ExtractNegativeNumbers(IEnumerable<int> numbers)
+        {
+            return numbers.Where(n => n < 0);
+        }
+
+        private static string RemoveCustomDelimitorDeclarationFrom(string input)
+        {
+            int endOfCustomDelimitorDeclarationIndex = 4;
+            int endOfSquareBracketIndex = input.IndexOf(']');
+            if (endOfSquareBracketIndex != -1)
+            {
+                endOfCustomDelimitorDeclarationIndex = endOfSquareBracketIndex + 2;
+            }
+            input = input.Substring(endOfCustomDelimitorDeclarationIndex);
+            return input;
+        }
+
+        private static IList<string> GetStandardDelimitors()
+        {
+            return new List<string>() { ",", "\n" };
+        }
+
+        private static bool HasNegativeNumbers(IEnumerable<int> negativeNumbers)
+        {
+            return negativeNumbers.Any();
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static bool HasCustomDelimitors(this string input)
+        {
+            return input.StartsWith("//");
+        }
+
+        public static IEnumerable<int> ExtractNumbers(this string input, IEnumerable<string> delimitors)
+        {
+            return input
+                .Split(delimitors.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+        }
+    }
+
+    internal static class DelimitorExtension
+    {
+        public static void RegisterCustomDelimitors(this IList<string> delimitors, string input)
+        {
+            int startOfSquareBracketIndex = input.IndexOf('[');
+            if (startOfSquareBracketIndex != -1)
+            {
+                int endOfSquareBracketIndex = input.IndexOf(']');
+                delimitors.Add(
+                    input.Substring(
+                        startOfSquareBracketIndex + 1,
+                        endOfSquareBracketIndex - startOfSquareBracketIndex -1));
+            }
+            else
+            {
+                const int delimitorIndex = 2;
+                delimitors.Add(input[delimitorIndex].ToString());
+            }
+        }
+    }
+}
--- StringCalculator.Test/StringCalculatorTests.cs
+++ StringCalculator.Test/StringCalculatorTests.cs
@@ -33,42 +33,63 @@ namespace StringCalculator.Test
             Assert.That(result, Is.EqualTo(expectedResult));
         }
 
-        [Test]
-        public void Should_return_sum_When_three_numbers_given()
+        [TestCase("1,2,3", 6)]
+        [TestCase("1,2,3,4", 10)]
+        public void Should_return_sum_When_unknown_amount_of_numbers_given(string input, int expectedSum)
         {
             StringCalculator stringCalculator = new StringCalculator();
-            int result = stringCalculator.Add("1,2,3");
-            Assert.That(result, Is.EqualTo(6));
+            int result = stringCalculator.Add(input);
+            Assert.That(result, Is.EqualTo(expectedSum));
         }
 
-    }
-
-    public class StringCalculator
-    {
-        public int Add(string input)
+        [Test]
+        public void Should_return_sum_when_newline_as_separator_and_two_numbers_provided()
         {
-            if (input.Length == 0)
-            {
-                return 0;
-            }
-            int commaIndex = input.IndexOf(",");
-            if (commaIndex != -1)
-            {
-                int sum = int.Parse(input.Substring(0, commaIndex));
-                string remainder = input.Substring(commaIndex + 1);
-                commaIndex = remainder.IndexOf(",");
-                if (commaIndex != -1)
-                {
-                    sum += int.Parse(remainder.Substring(0, commaIndex));
-                    sum += int.Parse(remainder.Substring(commaIndex + 1));
-                }
-                else
-                {
-                    sum += int.Parse(remainder);
-                }
-                return sum;
-            }
-            return int.Parse(input);
+            StringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("1\n2");
+            Assert.That(result, Is.EqualTo(3));
         }
-    }
+
+        [Test]
+        public void Should_accept_custom_delimiters_at_the_start_of_input()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("//;\n1;2");
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [TestCase("-2", "negatives not allowed: -2")]
+        [TestCase("-2,-3", "negatives not allowed: -2, -3")]
+        public void Should_throw_exception_with_explicit_message_When_negative_numbers_provided(string input, string expectedErrorMessage)
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            Assert.That(() => stringCalculator.Add(input), Throws.Exception.With.Message.EqualTo(expectedErrorMessage));
+        }
+
+        [Test]
+        public void Should_Ignore_numbers_higher_than_one_thousand_When_present_in_input()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("1001");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Should_accept_custom_delimiters_in_square_brackets()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("//[*]\n1*2");
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Should_accept_custom_delimiters_of_any_size_in_square_brackets()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+            int result = stringCalculator.Add("//[***]\n1***2");
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+    }   
 }
+
